6c694bd107fba2bc37dc7b0ec1a48cb2d56c486d
omar11011 <atarama11011@gmail.com>
2021-09-17 06:58:17 -0500

    Fixed CSS

--- src/public/js/navbar.js
+++ src/public/js/navbar.js
@@ -1,6 +1,4 @@
 const name_page = document.title.split(":").pop().toLowerCase().trim()
 const element = document.getElementById('nav-option-' + name_page)
 
-element.className += 'active'
-element.style.color = 'white'
-element.style.textDecoration = 'none'
+element.style.opacity = 1
